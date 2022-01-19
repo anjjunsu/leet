c769c2a6099c524c9be4093b6a16368c4a266242
anjjunsu <anjjunsu@gmail.com>
2022-01-19 01:17:43 -0800

    skel

---
+++ journey_log/containsDuplicate.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+class ContainsDuplicate {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+
+    }
+};
+
+int main() {
+    ContainsDuplicate demo;
+    
+    return 0;
+}
