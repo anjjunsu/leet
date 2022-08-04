44a490aad9600639f7b4c0b2ae550ca7a92c270d
Junsu An <anjjunsu@gmail.com>
2022-08-03 22:48:55 -0700

    Number of 1 bits - 191

--- journey_log/general/RemoveElement27.java
+++ journey_log/general/RemoveElement27.java
@@ -5,6 +5,19 @@ class Solution
         int left = 0;
         int right = nums.length - 1;
         
-
+        while (left <= right) 
+        {
+            if (nums[left] == val) 
+            {
+                int tmp = nums[left];
+                nums[left] = nums[right];
+                nums[right] = tmp;
+                right--;
+            }
+            else {
+                left++;
+            }
+        }
+        return left;
     }
 }
